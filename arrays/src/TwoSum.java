3296cf4d652815e494e99b93a0ae3c2287fe16f7
Deepak Joshi <deepak.joshi@ihsmarkit.com>
2021-03-09 14:45:55 +0530

    Two sum problem -2

--- arrays/src/TwoSum.java
+++ arrays/src/TwoSum.java
@@ -1,17 +1,18 @@
 import java.util.*;
 
-public class Q1_TwoSum {
+public class TwoSum {
     public static void main(String[] args) {
 /*
+        #1
         Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
         For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
         Bonus: Can you do this in one pass?
 */
         List<Integer> list = Arrays.asList(10, 15, 3, 7);
-        Integer k = 12;
+        Integer k = 17;
         boolean result = solve1(list, k);
         System.out.println(String.format("Result is: %b", result));
-
+        System.out.println(String.format("Result is: %b", solve2(list, k)));
     }
 
     public static boolean solve2(List<Integer> list, Integer k) {
